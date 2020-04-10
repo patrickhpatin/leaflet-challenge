d88071c8b3f6839b061efe3d4cd53acd339a3ae5
Patrick Patin <40468092+patrickhpatin@users.noreply.github.com>
2020-04-10 07:38:41 -0400

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+// Past 30 Days
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
+
+// Past 7 Days
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+// Past Day
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
